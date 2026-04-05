32768ee511c2bbfdbbf23d8332b84485c4401823
Bisher <bisher.madi84@gmail.com>
2026-04-06 01:22:27 +0400

    Add isSquare, isSymmetric, and isIdentity methods to Matrix class

--- src/Matrix.h
+++ src/Matrix.h
@@ -26,10 +26,45 @@ public:
     {
         return matrix_Rows;
     }
+
     int getCols() const
     {
         return matrix_Cols;
     }
+
+    bool isSquare() const
+    {
+        return matrix_Rows == matrix_Cols;
+    }
+    bool isSymmetric() const
+    {
+        if (!isSquare())
+            return false;
+
+        for (int row = 1; row < matrix_Rows; row++)
+            for (int col = 0; col < row; col++)
+                if (matrix_Data[row][col] != matrix_Data[col][row])
+                    return false;
+
+        return true;
+    }
+    bool isIdentity() const
+    {
+        if (!isSquare())
+            return false;
+
+        for (int row = 0; row < matrix_Rows; row++)
+            for (int col = 0; col < matrix_Cols; col++)
+                if (row != col && matrix_Data[row][col] != 0)
+                    return false;
+
+        for (int row = 0; row < matrix_Rows; row++)
+            if (matrix_Data[row][row] != 1)
+                return false;
+
+        return true;
+    }
+
     void print() const
     {
         for (int row = 0; row < matrix_Rows; row++)
